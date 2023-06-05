7bc6e5ad4cc9158d69a122192984a6814c7593bf
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-06-05 14:42:41 +0900

    ルーティングのみcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get "posts", to: "posts#index"
+  get "post", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "posts/new", to:"posts#new"
   # Defines the root path route ("/")
